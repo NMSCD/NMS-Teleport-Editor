e9b8a84eb0d60e8fa212adceb4e29a18aa673a1b
Lenni009 <guentherhanslphdyt@gmail.com>
2024-07-20 20:38:33 +0200

    remove excess stations to stay in limit

--- src/components/CopyButton.vue
+++ src/components/CopyButton.vue
@@ -1,19 +1,35 @@
 <script setup lang="ts">
 import { useEndpointDataStore } from '@/store/endpointData';
+import type { TeleportEndpoint, TeleporterTypes } from '@/types/teleportEndpoint';
+import { maxStations } from '@/variables/limits';
 import { storeToRefs } from 'pinia';
 import { ref } from 'vue';
 
 const endpointData = useEndpointDataStore();
-const { json, addedEndpoints } = storeToRefs(endpointData);
+const { json, addedEndpoints, typeCounter } = storeToRefs(endpointData);
 
 const defaultText = 'Copy modified JSON';
 const buttonText = ref(defaultText);
 const copied = ref(false);
 
+function getExcessEndpoints(arr: TeleportEndpoint[], type: TeleporterTypes) {
+  const endpointCount = typeCounter.value[type] ?? 0;
+  const excessEndpoints = Math.max(endpointCount - maxStations, 0);
+  const endpoints = arr.filter((item) => item.TeleporterType === type);
+  const removeStations = endpoints.slice(0, excessEndpoints);
+  return removeStations;
+}
+
 function copyJson() {
   const indent = 2;
   const displayTime = 1500;
-  const combinedEndpoints = [...json.value, ...addedEndpoints.value];
+  const excessStations = getExcessEndpoints(json.value, 'Spacestation');
+  const excessStationsFixPos = getExcessEndpoints(json.value, 'SpacestationFixPosition');
+  const filteredEndpoints = json.value.filter(
+    (item) => !excessStations.includes(item) && !excessStationsFixPos.includes(item)
+  );
+  const combinedEndpoints = [...filteredEndpoints, ...addedEndpoints.value];
+
   navigator.clipboard.writeText(JSON.stringify(combinedEndpoints, null, indent));
   buttonText.value = 'Copied!';
   copied.value = true;
